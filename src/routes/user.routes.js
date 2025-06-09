6d5d5ea5829afe2ec75ae80d2cbe74a14d7e4afd
Shreyash Rajurkar <shreyashraj712004@gmail.com>
2025-06-09 11:44:18 +0530

    login route started working properly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,26 +4,26 @@ import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
-const router = Router() 
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name : "avatar" , 
+            name: "avatar",
             maxCount: 1
-        } ,
+        }, 
         {
             name: "coverImage",
-            maxCount : 1
+            maxCount: 1
         }
-    ]) 
-    ,registerUser)
+    ]),
+    registerUser
+    )
 
-router.route("/login").post(registerUser)
+router.route("/login").post(loginUser)
 
-
-// secured routes
-router.route("/logout").post( verifyJWT , logoutUser)
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 
 
 export default router 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,28 +1,31 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs" 
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 
-// Configuration
+
 cloudinary.config({ 
-    cloud_name:  process.env.CLOUDINARY_CLOUD_NAME , 
-    api_key: process.env.CLOUDINARY_API_KEY , 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-const uploadCloudinary = async(localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null ; 
-        // uplad the file on cloudinary
-        const uploadResult = await cloudinary.uploader.upload(localFilePath , {
-            resource_type : "auto"
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         })
-        //file has been upladed succesfully
-        // console.log("File is uploaded on cloudinary" , uploadResult.url);
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
-        return uploadResult ; 
+        return response;
+
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the uplad operation got failed 
-        return null ;
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
     }
 }
 
-export {uploadCloudinary} 
+
+
+export {uploadOnCloudinary}
